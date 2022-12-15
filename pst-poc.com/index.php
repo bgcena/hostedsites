85c5cf025e05746b23655018b74ca16d81213a6a
chinmay <bgcena@gmail.com>
2022-12-15 23:54:19 +0530

    Update index.php
    added css and styles bootstrap

--- pst-poc.com/index.php
+++ pst-poc.com/index.php
@@ -1,16 +1,19 @@
 <!DOCTYPE html>
 <html>
 <head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 <title>Chinmay Test Page</title>
 </head>
 <body>
 
-<h1>Request details -Chinmay</h1>
+<h3>Request details -Chinmay</h3>
 </hr>
-<p>Request URL is: </p>
+<p class="navbar-text" >Request URL is: </p>
 <?php echo $actual_link = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";  ?>
 </hr>
-<p>These are request headers</p>
+<p class="navbar-text" >These are request headers</p>
 <?php
 foreach ($_SERVER as $key => $value) {
     if (strpos($key, 'HTTP_') === 0) {
@@ -26,6 +29,7 @@ foreach ($_SERVER as $key => $value) {
 
 ?>
 
-
+<!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
 </html>
