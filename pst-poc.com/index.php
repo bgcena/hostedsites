f8321cc72fabca7205148a4f982645cb8cc2622d
chinmay <bgcena@gmail.com>
2022-12-15 23:57:00 +0530

    Update index.php
    added container

--- pst-poc.com/index.php
+++ pst-poc.com/index.php
@@ -6,7 +6,7 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 <title>Chinmay Test Page</title>
 </head>
-<body>
+<body class="container">
 
 <h3>Request details -Chinmay</h3>
 </hr>
