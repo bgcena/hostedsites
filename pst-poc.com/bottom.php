dbb5333212c0608634c411fd901c2f5e224b9ed3
chinmay <bgcena@gmail.com>
2022-12-22 16:10:18 +0530

    added templated top and bottom

---
+++ pst-poc.com/bottom.php
@@ -0,0 +1,8 @@
+<p>Designed and maintained by Chinmay.  Contributed by Swamy</p>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <!-- Latest compiled and minified JavaScript -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+  </body>
+</html>
