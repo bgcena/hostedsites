dc51a140b0418b96e3a6688501684b03eaf6c5d8
chinmay <bgcena@gmail.com>
2022-12-22 15:38:59 +0530

    added request details page

---
+++ request-details/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+<title>Chinmay Test Page</title>
+</head>
+<body class="container">
+
+<h3>Request details -Chinmay</h3>
+</hr>
+<div class="bs-example">
+<p class="navbar-text" >Request URL is: </p>
+<?php echo $actual_link = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";  ?>
+</div>
+</hr>
+<div class="bs-example">
+<p class="navbar-text" >These are request headers</p>
+<?php
+foreach ($_SERVER as $key => $value) {
+    if (strpos($key, 'HTTP_') === 0) {
+        $chunks = explode('_', $key);
+        $header = '';
+        for ($i = 1; $y = sizeof($chunks) - 1, $i < $y; $i++) {
+            $header .= ucfirst(strtolower($chunks[$i])).'-';
+        }
+        $header .= ucfirst(strtolower($chunks[$i])).': '.$value;
+        echo $header.'<br>';
+    }
+}
+
+?>
+</div>
+<!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+</body>
+</html>
