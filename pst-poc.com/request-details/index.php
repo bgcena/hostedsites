6b5b6eac146315447687e4457bdb72f31df4f030
chinmay <bgcena@gmail.com>
2022-12-22 16:44:18 +0530

    Update index.php

--- pst-poc.com/request-details/index.php
+++ pst-poc.com/request-details/index.php
@@ -1,19 +1,4 @@
 <?php include "../top.php" ?>
-
-<nav class="navbar navbar-inverse navbar-fixed-top">
-    <div class="container">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="/index.php">Team PST</a>
-        </div>
-        <!--/.navbar-collapse -->
-    </div>
-</nav>
 <div class="container">
     <h3>Request details -Chinmay</h3>
     <table class="table">
